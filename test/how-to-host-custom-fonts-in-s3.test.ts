321f85fb515c11fb5ea6a325e667f6f88e4ba422
Alex Kates <alex.kates@croissant.com>
2023-11-24 07:15:55 -0500

    Initial commit

---
+++ test/how-to-host-custom-fonts-in-s3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HowToHostCustomFontsInS3 from '../lib/how-to-host-custom-fonts-in-s3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/how-to-host-custom-fonts-in-s3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HowToHostCustomFontsInS3.HowToHostCustomFontsInS3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
